670c2742cfae3823dec83ed8a03def4a66de50c1
lxg <lxg1986@gmail.com>
2015-02-25 22:24:50 +0800

    init the game

--- cavas/bee-canvas.js
+++ cavas/bee-canvas.js
@@ -10,4 +10,27 @@ window.RAF = (function(){
         function(callback){
             window.setTimeout(callback, 1000 / 60);
         }
-})();
+}());
+var BEE = (function(){
+    var imgSrc = [
+        "images/bg.png","images/bullet.png","images/bullet_enemy.png",
+        "images/enemy.png","images/ship.png"
+    ]
+    ,extend = function(child,parent){
+        var F = function(){}
+        F.prototype = parent.prototype
+        child.prototype = new F()
+        child.prototype.constructor = child
+    }
+    ,imageOnload = function(){
+        
+    };
+
+    return {
+        init : function(){
+            var imgObj = [],len = imgSrc.length
+        }
+    }
+}());
+
+BEE.init()
