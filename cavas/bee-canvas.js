77eb8d5ce05134f898b856405fe0b063ff620383
lxg <lxg1986@gmail.com>
2015-02-25 21:44:41 +0800

    rewrite this game

---
+++ cavas/bee-canvas.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lxg on 15-2-25.
+ */
+window.RAF = (function(){
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        }
+})();
