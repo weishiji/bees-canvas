79516e84452cf5ef8e31c961101db4cd229ad9b7
刘小光 <liuxiaoguang@zihua.com.cn>
2015-02-26 18:57:45 +0800

    loadbar

--- cavas/bee-canvas.js
+++ cavas/bee-canvas.js
@@ -46,37 +46,52 @@ var BEE = (function(){
     	return this.canvas.height	
     }
     DrawCanvas.prototype.clearCanvas = function(){
-    	this.ctx.clearRect(this.x,this.y,this.getCanvasWidth(),this.getCnavasHeight())
+    	this.ctx.clearRect(0,0,this.getCanvasWidth(),this.getCnavasHeight())
     }
 
     //loadbar
     function LoadBar(){
     	DrawCanvas.call(this)
+        this.startDeg = 0
     }
     extend(LoadBar,DrawCanvas)
     LoadBar.prototype.loadImage = function(fun){
-    	var len = 0,tempSrc = [],itemArr = [],imageObject = {}
+    	var len = 0,srcArr = [],itemArr = [],imageObject = {},that = this
     	for(var item in imageSrc){
 			itemArr.push(item)
-    		tempSrc.push(imageSrc[item])
+    		srcArr.push(imageSrc[item])
     	}
-    	function _loadImage(tempSrc){
-    		var temp = tempSrc.splice(0,1)
-    		if(temp.length === 0){
-				fun(imageObject)
-    			return	
-    		} 
+    	function _loadImage(srcArr){
+    		var temp = srcArr.splice(0,1)
+    		if(temp.length === 0) return; 
     		var img = new Image()
             img.src = temp
             img.onload = function(){
-            	imageObject[itemArr.splice(0,1)] = this
-	    		_loadImage(tempSrc)
+            	imageObject[itemArr[len]] = this
+	    		_loadImage(srcArr)
+                that.createCircle( len * 100 / itemArr.length)
+                len += 1
+                if(len === itemArr.length){
+                    that.createCircle(100)
+                    fun(imageObject)
+                }
             }
     	}
-    	_loadImage(tempSrc)
+    	_loadImage(srcArr)
     }
-    LoadBar.prototype.createCircle = function(){
-    	
+    LoadBar.prototype.createCircle = function(degNum){
+    	var centerX = this.getCanvasWidth() / 2
+    		,centerY = this.getCnavasHeight() / 2
+    		,radius = 60,deg = Math.PI * (2 * degNum / 100)
+
+        this.clearCanvas()
+    	this.ctx.beginPath()
+        this.ctx.moveTo(centerX,centerY)
+    	this.ctx.arc(centerX,centerY,radius,this.startDeg,deg,false);
+        this.ctx.lineTo(centerX,centerY)
+    	this.ctx.fillStyle = 'green';
+        this.ctx.fill();
+        this.startDeg = deg
     }
     //sky
     function Background(){
@@ -86,9 +101,19 @@ var BEE = (function(){
     return {
         init : function(){
         	var loader = new LoadBar()
-        	loader.loadImage(function(imageObj){
-        		console.log(imageObj)
-        	})
+        	//loader.loadImage(function(imageObj){
+              //  console.log(imageObj)
+        	//})
+            var i = 0,timer;
+            timer = setInterval(function(){
+                i += 1
+                loader.createCircle(i)
+                console.log(i)
+                if(i > 100){
+                    clearInterval(timer)
+                }
+            },10)
         }
+
     }
 }());
