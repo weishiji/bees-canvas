8b2bc1aef5d9d3d5a239dc0064defe6f61259d2d
lxg <lxg1986@gmail.com>
2015-02-28 00:00:04 +0800

    add event listener with canva shape

--- cavas/bee-canvas.js
+++ cavas/bee-canvas.js
@@ -26,13 +26,23 @@ var BEE = (function(){
         child.prototype.constructor = child
     };
     var Stage = (function(){
+
         var _bindEvt = function(){
             var that = this
             that.canvas.addEventListener('click',function(ev){
-                for(var item in that.events){
+                var clickedX = ev.pageX - this.offsetLeft
+                    ,clickedY = ev.pageY - this.offsetTop;
+                for(var i=0;i<that.shapesArr.length;i+=1){
+                    var temp = that.shapesArr[i]
+                    if (clickedX < temp.right && clickedX > temp.left && clickedY > temp.top && clickedY < temp.bottom) {
+                        alert ('clicked number ' + (i + 1));
+                        return
+                    }
+                }
+                /*for(var item in that.events){
                     var temp = that.events[item]
                     temp()
-                }
+                }*/
             })
         }
         function stage(){
@@ -59,14 +69,48 @@ var BEE = (function(){
         return new stage()
     })
     var Shape = (function(){
-
         function shape(stage,x,y){
             this.stage = stage
             this.x = x
             this.y = y
         }
+        shape.prototype.createCircle = function(){
+            var x = this.x,y = this.y,radius = this.radius
+            this.left = x - radius;
+            this.top = y - radius;
+            this.right = x + radius;
+            this.bottom = y + radius;
+        }
         return shape
     }())
+    function Circle(stage,x,y,radius){
+        Shape.call(this,stage,x,y)
+        this.radius = radius
+        this.createCircle()
+        this.draw()
+        this.stage.addShape(this)
+    }
+    extend(Circle,Shape)
+    Circle.prototype.draw = function(){
+        var context = this.stage.ctx
+            ,x = this.x
+            ,y = this.y
+            ,radius = this.radius
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'green';
+        context.fill();
+        context.lineWidth = 1;
+        context.strokeStyle = '#333';
+        context.stroke();
+
+        context.fillStyle = '#fff';
+        context.textAlign = 'center';
+
+        context.fillText('start', x, y);
+    }
+
+
     function Sky(stage,x,y){
         Shape.call(this,stage,x,y)
         this.stage.addShape(this)
@@ -74,13 +118,14 @@ var BEE = (function(){
     }
     extend(Sky,Shape)
     Sky.prototype.test = function(){
+        this.stage.ctx.fillRect(this.x,this.y,40,40)
         console.log(this.stage.shapesArr)
     }
     ;return {
         init : function(){
             var stage = Stage()
-            new Sky(stage,10,10)
-            
+            new Circle(stage,100,100,20)
+            new Circle(stage,120,110,30)
         }
 
     }
